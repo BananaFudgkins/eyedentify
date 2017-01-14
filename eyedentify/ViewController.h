d2fbaa97fd5a6078492ad41541568acb87d1db06
Michael Royzen <michael@mlabtechnologies.com>
2017-01-14 15:51:36 -0800

    Intial commit

---
+++ eyedentify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eyedentify
+//
+//  Created by Michael Royzen on 1/14/17.
+//  Copyright © 2017 The Three Bruhsketeers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eyedentify/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  eyedentify
+//
+//  Created by Michael Royzen on 1/14/17.
+//  Copyright © 2017 The Three Bruhsketeers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
