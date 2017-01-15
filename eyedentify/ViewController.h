91b7f29b5fbd0c35bd1a58e07a000f56b4a0ed6b
Michael Royzen <michael@mlabtechnologies.com>
2017-01-15 03:45:20 -0800

    Staged

---
+++ eyedentify/ViewController.h
@@ -0,0 +1,42 @@
+//
+//  ViewController.h
+//  eyedentify
+//
+//  Created by Michael Royzen on 1/14/17.
+//  Copyright © 2017 The Three Bruhsketeers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "OverlayViewController.h"
+#import <CoreImage/CoreImage.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <ImageIO/ImageIO.h>
+#import <SpeechKit/SpeechKit.h>
+#import "eyedentify-Swift.h"
+#import <MetalPerformanceShaders/MetalPerformanceShaders.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "FGTranslator.h"
+#import "AFNetworking.h"
+#import "KVNProgress.h"
+
+@interface ViewController : UIViewController <AVSpeechSynthesizerDelegate, SpeechKitDelegate, SKRecognizerDelegate, UIGestureRecognizerDelegate> {
+    SKRecognizer *voiceSearch;
+    enum {
+        TS_IDLE,
+        TS_INITIAL,
+        TS_RECORDING,
+        TS_PROCESSING,
+    } transactionState;
+}
+
+@property(nonatomic, retain) AVCaptureStillImageOutput *stillImageOutput;
+@property(nonatomic, retain) IBOutlet UIImageView *vImage;
+@property (strong, nonatomic) IBOutlet UIButton *fullScreenButton;
+@property (nonatomic) BOOL shouldRevert;
+@property (strong, nonatomic) IBOutlet UILabel *recognizedObjectLabel;
+
+@end
+
---
+++ eyedentifyTests/eyedentifyTests.m
@@ -0,0 +1,39 @@
+//
+//  eyedentifyTests.m
+//  eyedentifyTests
+//
+//  Created by Michael Royzen on 1/14/17.
+//  Copyright © 2017 The Three Bruhsketeers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface eyedentifyTests : XCTestCase
+
+@end
+
+@implementation eyedentifyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
