2da37dd75c3ecfba1c42d55345d70e67fcb48c9b
Deja Jackson <bananafudgkins@gmail.com>
2017-01-14 17:19:31 -0800

    Moved the full screen button to the main view controller

--- eyedentify/OverlayViewController.m
+++ eyedentify/OverlayViewController.m
@@ -18,6 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.iconBackground.layer.cornerRadius = self.iconBackground.frame.size.width / 2;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(animateEverything)
+                                                 name:@"animateUp"
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,8 +30,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)fullScreenPressed:(id)sender {
-    NSLog(@"The full screen button was tapped.");
+- (void)animateEverything {
+    
 }
 
 /*
